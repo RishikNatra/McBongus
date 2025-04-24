00f854d8255afddc261e8f304e5037df61485fc0
RishikNatra <rishik.natra.work@gmail.com>
2025-04-24 11:03:26 +0530

    Cleaning up

--- Backend/config.py
+++ Backend/config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "Your Password here",
+    "password": "Rishik@1429145",
     "database": "mcbongus_db",
     "port" : 3306
 }
